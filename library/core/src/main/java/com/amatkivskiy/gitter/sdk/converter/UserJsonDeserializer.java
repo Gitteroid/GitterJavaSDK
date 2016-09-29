418bb9c7ca3e2c02ae31c10b0d040b9ca024f724
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2016-09-29 16:05:33 +0300

    Add missing properties to UserResponse. Resolve #15 and #17.

--- library/core/src/main/java/com/amatkivskiy/gitter/sdk/converter/UserJsonDeserializer.java
+++ library/core/src/main/java/com/amatkivskiy/gitter/sdk/converter/UserJsonDeserializer.java
@@ -1,15 +1,21 @@
 package com.amatkivskiy.gitter.sdk.converter;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import com.amatkivskiy.gitter.sdk.model.response.Role;
 import com.amatkivskiy.gitter.sdk.model.response.UserResponse;
-import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
 public class UserJsonDeserializer implements JsonDeserializer<UserResponse> {
   @Override
   public UserResponse deserialize(JsonElement jsonElement, Type type,
-                                  JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-    JsonObject element = null;
+                                  JsonDeserializationContext deserializer) throws JsonParseException {
+    JsonObject element;
     if (jsonElement.isJsonArray()) {
       element = jsonElement.getAsJsonArray().get(0).getAsJsonObject();
     } else {
@@ -20,11 +26,23 @@ public class UserJsonDeserializer implements JsonDeserializer<UserResponse> {
     Integer v = element.get("v") != null ? element.get("v").getAsInt() : null;
     String username = element.get("username") != null ? element.get("username").getAsString() : null;
     String avatarUrlSmall = element.get("avatarUrlSmall") != null ? element.get("avatarUrlSmall").getAsString() : null;
+    String avatarUrl = element.get("avatarUrl") != null ? element.get("avatarUrl").getAsString() : null;
     String avatarUrlMedium = element.get("avatarUrlMedium") != null ? element.get("avatarUrlMedium").getAsString() : null;
     String gv = element.get("gv") != null ? element.get("gv").getAsString() : null;
     String displayName = element.get("displayName") != null ? element.get("displayName").getAsString() : null;
     String url = element.get("url") != null ? element.get("url").getAsString() : null;
+    boolean staff = element.get("staff") != null && element.get("staff").getAsBoolean();
 
-    return new UserResponse(id, v, username, avatarUrlSmall, gv, displayName, url, avatarUrlMedium);
+    Role role;
+    if (element.get("role") != null) {
+      role = deserializer.deserialize(element.get("role"), Role.class);
+      if (role == null) {
+        role = Role.STANDARD;
+      }
+    } else {
+      role = Role.STANDARD;
+    }
+
+    return new UserResponse(id, v, username, avatarUrl, avatarUrlSmall, gv, displayName, url, avatarUrlMedium, role, staff);
   }
 }
--- library/core/src/main/java/com/amatkivskiy/gitter/sdk/model/response/UserResponse.java
+++ library/core/src/main/java/com/amatkivskiy/gitter/sdk/model/response/UserResponse.java
@@ -5,20 +5,26 @@ public class UserResponse {
   public final Integer v;
   public final String username;
   public final String displayName;
+  public final String avatarUrl;
   public final String avatarUrlSmall;
   public final String avatarUrlMedium;
+  public final Role role;
+  public final boolean staff;
   public final String gv;
   public final String url;
 
-  public UserResponse(String id, Integer v, String username, String avatarUrlSmall, String gv, String displayName, String url, String avatarUrlMedium) {
+  public UserResponse(String id, Integer v, String username, String avatarUrl, String avatarUrlSmall, String gv, String displayName, String url, String avatarUrlMedium, Role role, boolean staff) {
     this.id = id;
     this.v = v;
     this.username = username;
+    this.avatarUrl = avatarUrl;
     this.avatarUrlSmall = avatarUrlSmall;
     this.gv = gv;
     this.displayName = displayName;
     this.url = url;
     this.avatarUrlMedium = avatarUrlMedium;
+    this.role = role;
+    this.staff = staff;
   }
 
   @Override
@@ -28,8 +34,11 @@ public class UserResponse {
         ", v=" + v +
         ", username='" + username + '\'' +
         ", displayName='" + displayName + '\'' +
+        ", avatarUrl='" + avatarUrl + '\'' +
         ", avatarUrlSmall='" + avatarUrlSmall + '\'' +
         ", avatarUrlMedium='" + avatarUrlMedium + '\'' +
+        ", role=" + role +
+        ", staff=" + staff +
         ", gv='" + gv + '\'' +
         ", url='" + url + '\'' +
         '}';
